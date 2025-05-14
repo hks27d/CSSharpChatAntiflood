c978c82daa63f264f3a1fea2155532c120bf16d0
hks27d <51793958+hks27d@users.noreply.github.com>
2025-05-14 19:54:26 +0100

    refactor: complete code overhaul

--- CSSharpChatAntiflood.cs
+++ CSSharpChatAntiflood.cs
@@ -2,30 +2,57 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
-using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Modules.Utils;
+using System.Collections.Concurrent;
 
 namespace CSSharpChatAntiflood
 {
+    public static class ConcurrentDictionaryExtensions
+    {
+        /// <summary>
+        /// Rebuilds ConcurrentDictionary to reduce internal memory usage
+        /// </summary>
+        /// <remarks>
+        /// This method can be used to minimize the memory overhead
+        /// once it is known that no new elements will be added.
+        /// 
+        /// The capacity reduction to the minimum is not guaranteed.
+        /// ConcurrentDictionary has a built-in capacity mechanism that
+        /// may still allocate some default minimum capacity for performance reasons
+        ///
+        /// To allocate minimum size storage array, execute the following statements:
+        ///
+        /// dictionary.Clear();
+        /// dictionary = dictionary.ReduceMemory();
+        /// </remarks>
+        public static ConcurrentDictionary<TKey, TValue> ReduceMemory<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> source)
+        where TKey : notnull
+        {
+            return new ConcurrentDictionary<TKey, TValue>(source);
+        }
+    }
+
     public class CSSharpChatAntiflood : BasePlugin
     {
         public override string ModuleName => "CS# Chat Antiflood";
-        public override string ModuleVersion => "1.0.0";
+        public override string ModuleVersion => "2.0.0";
         public override string ModuleAuthor => "HKS 27D";
         public override string ModuleDescription => "";
 
-#pragma warning disable IDE0044 // Add readonly modifier
-        Dictionary<int, int> PlayerWarnings = [];
-        Dictionary<int, bool> PlayerRootStatus = [];
-#pragma warning restore IDE0044 // Add readonly modifier
+        ConcurrentDictionary<int, bool> PlayersImmunity = [];
+        ConcurrentDictionary<int, long> PlayersTimestamp = [];
+        ConcurrentDictionary<int, byte> PlayersMessages = [];
 
-        readonly float ResetWarningsInterval = 5.0f;
-        readonly int MaxMessages = 3;
+        // Modify based on your preferences
+        // Make sure you set MaxMessages value >= 3
+        const byte MaxMessages = 3;
+        const long MinSecondsBetweenMessages = 5;
+        const string ChatAntifloodTAG = "Modders";
 
         public override void Load(bool hotReload)
         {
-            AddCommandListener("say", ChatAntifloodListener, HookMode.Pre);
-            AddCommandListener("say_team", ChatAntifloodListener, HookMode.Pre);
+            AddCommandListener("say", ChatListener, HookMode.Pre);
+            AddCommandListener("say_team", ChatListener, HookMode.Pre);
 
             RegisterEventHandler((EventPlayerConnectFull @event, GameEventInfo info) =>
             {
@@ -37,15 +64,14 @@ namespace CSSharpChatAntiflood
                 if (!Player.UserId.HasValue)
                     return HookResult.Continue;
 
-                var PlayerUserId = Player.UserId.Value;
                 if (AdminManager.PlayerHasPermissions(Player, "@css/root"))
                 {
-                    PlayerRootStatus.TryAdd(PlayerUserId, true);
+                    PlayersImmunity[Player.UserId.Value] = true;
                     return HookResult.Continue;
                 }
 
-                PlayerRootStatus.TryAdd(PlayerUserId, false);
-                PlayerWarnings.TryAdd(PlayerUserId, 0);
+                PlayersTimestamp[Player.UserId.Value] = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                PlayersMessages[Player.UserId.Value] = 0;
 
                 return HookResult.Continue;
             }, HookMode.Post);
@@ -60,74 +86,71 @@ namespace CSSharpChatAntiflood
                 if (!Player.UserId.HasValue)
                     return HookResult.Continue;
 
-                var PlayerUserId = Player.UserId.Value;
-                PlayerRootStatus.Remove(PlayerUserId);
-                PlayerWarnings.Remove(PlayerUserId);
+                PlayersImmunity.TryRemove(Player.UserId.Value, out _);
+                PlayersTimestamp.TryRemove(Player.UserId.Value, out _);
+                PlayersMessages.TryRemove(Player.UserId.Value, out _);
 
                 return HookResult.Continue;
             }, HookMode.Pre);
 
             RegisterListener<Listeners.OnMapStart>((mapName) =>
             {
-                AddTimer(1.0f, () =>
-                {
-                    PlayerRootStatus.Clear();
-                    PlayerWarnings.Clear();
-                    ReaddUserIDValues();
-                });
+                PlayersImmunity.Clear();
+                PlayersImmunity = PlayersImmunity.ReduceMemory();
+                PlayersTimestamp.Clear();
+                PlayersTimestamp = PlayersTimestamp.ReduceMemory();
+                PlayersMessages.Clear();
+                PlayersMessages = PlayersMessages.ReduceMemory();
+
+                AddUserIDValues();
             });
-            AddTimer(ResetWarningsInterval, ChatAntiflood_TimerCallback, TimerFlags.REPEAT);
         }
 
-        private void ReaddUserIDValues()
+        private void AddUserIDValues()
         {
             List<CCSPlayerController> TotalPlayers = Utilities.GetPlayers();
             foreach (CCSPlayerController Player in TotalPlayers)
             {
-                if (Player != null && Player.IsValid && Player.Connected == PlayerConnectedState.PlayerConnected && Player.UserId.HasValue)
+                if (Player != null && Player.IsValid && !Player.IsBot && !Player.IsHLTV && Player.Connected == PlayerConnectedState.PlayerConnected && Player.UserId.HasValue)
                 {
-                    var PlayerUserId = Player.UserId.Value;
                     if (AdminManager.PlayerHasPermissions(Player, "@css/root"))
-                        PlayerRootStatus.TryAdd(PlayerUserId, true);
-                    else
                     {
-                        PlayerRootStatus.TryAdd(PlayerUserId, false);
-                        PlayerWarnings.TryAdd(PlayerUserId, 0);
+                        PlayersImmunity[Player.UserId.Value] = true;
+                        continue;
                     }
+
+                    PlayersTimestamp[Player.UserId.Value] = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                    PlayersMessages[Player.UserId.Value] = 0;
                 }
             }
         }
 
-        private void ChatAntiflood_TimerCallback()
-        {
-            List<CCSPlayerController> players = Utilities.GetPlayers();
-            foreach (CCSPlayerController Player in players)
-            {
-                if (Player != null && Player.IsValid && Player.Connected == PlayerConnectedState.PlayerConnected && !Player.IsBot && Player.UserId.HasValue)
-                {
-                    var PlayerUserId = Player.UserId.Value;
-                    if (PlayerRootStatus.TryGetValue(PlayerUserId, out bool value) && value == false)
-                        PlayerWarnings[PlayerUserId] = 0;
-                }
-            }
-        }
-
-        private HookResult ChatAntifloodListener(CCSPlayerController? Player, CommandInfo info)
+        private HookResult ChatListener(CCSPlayerController? Player, CommandInfo info)
         {
             if (Player != null && Player.IsValid && Player.Connected == PlayerConnectedState.PlayerConnected && Player.UserId.HasValue)
             {
-                var PlayerUserId = Player.UserId.Value;
-                if (PlayerRootStatus.TryGetValue(PlayerUserId, out bool StatusValue) && StatusValue == true)
+                if (PlayersImmunity.ContainsKey(Player.UserId.Value))
                     return HookResult.Continue;
 
-                ++PlayerWarnings[PlayerUserId];
-                if (PlayerWarnings.TryGetValue(PlayerUserId, out int WarningsValue) && WarningsValue > MaxMessages)
+                if (PlayersTimestamp.TryGetValue(Player.UserId.Value, out long TimestampValue) && (DateTimeOffset.UtcNow.ToUnixTimeSeconds() - TimestampValue > MinSecondsBetweenMessages))
                 {
-                    Player.PrintToChat($" {ChatColors.LightRed}Modders {ChatColors.Default}- Stop {ChatColors.LightRed}spamming {ChatColors.Default}the chat!");
-                    return HookResult.Handled;
-                }
-            }
+                    PlayersTimestamp[Player.UserId.Value] = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                    PlayersMessages[Player.UserId.Value] = 0;
 
+                    return HookResult.Continue;
+                }
+
+                PlayersTimestamp[Player.UserId.Value] = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+                if (PlayersMessages.TryGetValue(Player.UserId.Value, out byte MessagesNumber) && MessagesNumber <= MaxMessages)
+                {
+                    PlayersMessages[Player.UserId.Value] = ++MessagesNumber;
+                    return HookResult.Continue;
+                }
+
+                Player.PrintToChat($" {ChatColors.LightRed}{ChatAntifloodTAG} {ChatColors.Default}- Stop {ChatColors.LightRed}spamming {ChatColors.Default}the chat!");
+                return HookResult.Handled;
+            }
             return HookResult.Continue;
         }
     }
